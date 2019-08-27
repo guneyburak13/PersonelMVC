5c8a9c73e59cb35448b32cd7ba3d90b81ec01fe0
Burak Güney <guneyburak13@gmail.com>
2019-08-27 15:35:36 +0300

    C# ile Personel Web
    SQL veri tabanlı personel bilgi sistemi için GRID işemli web sitesi

---
+++ PersonelMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonelMVC.Startup))]
+namespace PersonelMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
